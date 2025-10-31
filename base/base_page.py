aabf2b3f03371836e0ff0d52706241b4b33634bd
Tu Anh <dangtuanh89@gmail.com>
2025-10-31 20:39:23 +0700

    update

--- base/base_page.py
+++ base/base_page.py
@@ -9,20 +9,20 @@ class BasePage:
         self.driver = driver
 
     def get_element(self, locator):
-        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
+        element = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(locator))
         return element
     
     def get_elements(self, locator):
-        elements = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(locator))
+        elements = WebDriverWait(self.driver, 15).until(EC.presence_of_all_elements_located(locator))
         return elements
     
     def type(self, locator, text):
-        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
+        element = WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable(locator))
         element.clear()
         element.send_keys(text)
 
     def is_displayed(self, locator):
-        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
+        element = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(locator))
         return element.is_displayed()
 
     def click_by_js(self, locator):
